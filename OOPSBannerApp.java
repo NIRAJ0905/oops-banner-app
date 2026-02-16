e7b7defd872bd1d2657cea3d369490334690a8f7
NIRAJ0905 <nirajpingale005@gmail.com>
2026-02-16 18:17:52 +0530

    UC8: Added user input support for banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class OOPSBannerApp {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter text to print as banner: ");
+        String input = scanner.nextLine();
+
         Banner banner = new Banner();
-        banner.print("OOPS");
+        banner.print(input);
+
+        scanner.close();
     }
 }
