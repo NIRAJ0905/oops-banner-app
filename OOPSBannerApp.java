8f222a909aba0027b546b2d2c48c77737371cbe5
NIRAJ0905 <nirajpingale005@gmail.com>
2026-02-16 17:37:09 +0530

    UC1 refined: JavaDoc, class structure, console output

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ *
+ * This program prints the text "OOPS" to the console.
+ *
+ * @author Niraj
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method – entry point of the program
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
